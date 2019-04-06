42aa048f9ab65dfa24de03e67e145d5d409fb053
issa <issa@souktel.org>
2019-04-06 16:20:31 +0300

    add view for checkboxes

--- resources/views/includes/form-field-display.blade.php
+++ resources/views/includes/form-field-display.blade.php
@@ -3,6 +3,8 @@
     @case('numeric')
     @case('tel')
     @case('email')
+    @case('password')
+    @case('date')
 
         <div class="form-group @if(isset($display['width']))col-md-{{$display['width']}}@endif">
             <label for="{{$name}}">{{$title[session('locale', 'ar')]}}</label>
@@ -45,6 +47,24 @@
                 @endif
             @endforeach
             </ul>
+        </div>
+    </div>
+    @break
+
+    @case('boolean')
+    <div class="form-group @if(isset($display['width']))col-md-{{$display['width']}}@endif ">
+        <div class="custom-control custom-checkbox mt-3 mb-4">
+            <input
+                    @if(isset($order)) tabindex="{{$order}}"  @endif
+            type="checkbox" class="custom-control-input"  disabled name="{{$name}}" id="{{$name}}" value="1"
+                    @if(isset($validation['required']) && $validation['required'])required="required"@endif
+                    @if(isset($value) and $value == 1) checked="checked"  @endif
+            >
+
+
+
+
+            <label class="custom-control-label f-14" for="{{$name}}">{{$title[session('locale', 'ar')]}}</label>
         </div>
     </div>
     @break
